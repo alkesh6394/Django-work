4e1c08aa38ef105092d5b046605deca8e39a4c0a
alkesh <alkesh6394@gmail.com>
2023-10-18 20:03:35 +0530

    change sqlite to mysql

--- my_tennis_club/settings.py
+++ my_tennis_club/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'my_tennis_club.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'my_tennis_club',                
+        'USER': 'root',                          
+        'PASSWORD': '123456',                  
+        'HOST': '127.0.0.1',                     
+        'PORT': '3306',      
     }
 }
 
