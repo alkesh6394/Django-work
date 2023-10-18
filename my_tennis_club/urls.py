ba0ab1f32eb0bb0a82bc44e2c1eabb5fdf669f9f
alkesh <alkesh6394@gmail.com>
2023-10-18 18:56:15 +0530

    this is commit after change the views in class api-views

--- my_tennis_club/urls.py
+++ my_tennis_club/urls.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from app.views import EmployeeList, EmployeeDetail,EmployeeDeleteView,EmployeeUpdateView
+from app.views import EmployeeList, EmployeeDetail
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('employees/', EmployeeList.as_view(), name='employee-list'),
     path('employees/<int:pk>/', EmployeeDetail.as_view(), name='employee-detail'),
-    path('employees/<int:pk>/update/', EmployeeUpdateView.as_view(), name='employee-update'),
-    path('employees/<int:pk>/delete/', EmployeeDeleteView.as_view(), name='employee-delete'),
 ]
+
