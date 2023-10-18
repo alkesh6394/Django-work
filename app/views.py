dcc913cf47211e742f012bdf8a7169a55d70f060
alkesh <alkesh6394@gmail.com>
2023-10-18 19:33:50 +0530

    this is commit after class-view

--- app/views.py
+++ app/views.py
@@ -1,21 +1,55 @@
-from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 from .models import Employee
 from .serializers import EmployeeSerializer
-from rest_framework import permissions
 
-class EmployeeList(generics.ListCreateAPIView):
-    queryset = Employee.objects.all()
-    serializer_class = EmployeeSerializer
+class EmployeeList(APIView):
+    """
+    List all employees, or create a new employee.
+    """
+    def get(self, request, format=None):
+        employees = Employee.objects.all()
+        serializer = EmployeeSerializer(employees, many=True)
+        return Response(serializer.data)
 
-class EmployeeDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Employee.objects.all()
-    serializer_class = EmployeeSerializer
+    def post(self, request, format=None):
+        serializer = EmployeeSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class EmployeeUpdateView(generics.RetrieveUpdateAPIView):
-    queryset = Employee.objects.all()
-    serializer_class = EmployeeSerializer
+class EmployeeDetail(APIView):
+    """
+    Retrieve, update, or delete an employee instance.
+    """
+    def get_object(self, pk):
+        try:
+            return Employee.objects.get(pk=pk)
+        except Employee.DoesNotExist:
+            return None
 
-class EmployeeDeleteView(generics.DestroyAPIView):
-    queryset = Employee.objects.all()
-    serializer_class = EmployeeSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    def get(self, request, pk, format=None):
+        employee = self.get_object(pk)
+        if employee is None:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = EmployeeSerializer(employee)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        employee = self.get_object(pk)
+        if employee is None:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = EmployeeSerializer(employee, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        employee = self.get_object(pk)
+        if employee is None:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        employee.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
